496107f1a9214a6c67c82cc41baf7799d167352a
sstainbr <sstainbr@umich.edu>
2022-02-17 19:15:11 -0500

    Update RecordKeeper.cs

--- PatientRecordApplication/PatientRecordApplication/RecordKeeper.cs
+++ PatientRecordApplication/PatientRecordApplication/RecordKeeper.cs
@@ -14,37 +14,50 @@ namespace PatientRecordApplication
         private static StreamReader reader;
         public static void WritePatient(Patient patient)
         {
-            fileStream = new FileStream("PatientData.txt", FileMode.OpenOrCreate, FileAccess.Write);
-            writer = new StreamWriter(fileStream);
-            fileStream.Seek(fileStream.Length, SeekOrigin.Begin);
-            writer.WriteLine(patient.IDNum + "," + patient.Name + "," + patient.BalanceOwed);
-            writer.Close();
-            fileStream.Close();
+            try
+            {
+                fileStream = new FileStream("PatientData.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                writer = new StreamWriter(fileStream);
+                fileStream.Seek(fileStream.Length, SeekOrigin.Begin);
+                writer.WriteLine(patient.IDNum + "," + patient.Name + "," + patient.BalanceOwed);
+            }
+            finally {
+                writer.Close();
+                fileStream.Close();
+            }
         }
         public static void UpdatePatient(Patient patient)
         {
             string recordIn;
             string[] fields;
 
-            fileStream = new FileStream("PatientData.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-            writer = new StreamWriter(fileStream);
-            reader = new StreamReader(fileStream);
-            fileStream.Seek(0, SeekOrigin.Begin);
+            try {
+                fileStream = new FileStream("PatientData.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                writer = new StreamWriter(fileStream);
+                reader = new StreamReader(fileStream);
+                fileStream.Seek(0, SeekOrigin.Begin);
 
-            recordIn = reader.ReadLine();
-            while (recordIn != null)
-            {
-                fields = recordIn.Split(',');
-                if (Convert.ToInt32(fields[0]) == patient.IDNum)
-                {
-                    writer.WriteLine("TESTESTTESTESTEST");
-                    //writer.WriteLine(patient.IDNum + "," + patient.Name + "," + patient.BalanceOwed);
-                    return;
-                }
                 recordIn = reader.ReadLine();
+                while (recordIn != null)
+                {
+                    fields = recordIn.Split(',');
+                    if (Convert.ToInt32(fields[0]) == patient.IDNum)
+                    {
+                        writer.WriteLine("TESTESTTESTESTEST");
+                        //writer.WriteLine(patient.IDNum + "," + patient.Name + "," + patient.BalanceOwed);
+                        return;
+                    }
+                    recordIn = reader.ReadLine();
+                }
+                //Only reached if the patient is new
+                WritePatient(patient);
             }
-            //Only reached if the patient is new
-            WritePatient(patient);
+            finally
+            {
+                writer.Close();
+                reader.Close();
+                fileStream.Close();
+            }
         }
     }
 }
